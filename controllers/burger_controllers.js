c8b01eee0948a7bb72d91999a371f0ef9e517cc9
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-10-16 20:33:10 -0400

    finish

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
+// Create all routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
         var hbsObject = {
